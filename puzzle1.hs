51e2b4bac2ae6fb5c66b240449cc66e8e817b3b0
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2020-12-01 00:24:46 -0500

    puzzle 1

---
+++ puzzle1.hs
@@ -0,0 +1,24 @@
+import System.IO
+
+cartProd :: [Int] -> [Int] -> [(Int, Int)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+cartProd3 :: [Int] -> [Int] -> [Int] -> [(Int, Int, Int)]
+cartProd3 xs ys zs = [(x, y, z) | x <- xs, y <- ys, z <- zs]
+
+f :: [String] -> [Int]
+f = map read
+
+multTupleElements :: (Int, Int) -> Int
+multTupleElements (a,b) = a*b
+
+multTupleElements3 :: (Int, Int, Int) -> Int
+multTupleElements3 (a,b, c) = a*b*c
+
+main = do
+    handle <- openFile "puzzle1a.txt" ReadMode
+    contents <- hGetContents handle
+    let input = (f (lines contents))
+    putStrLn (show (multTupleElements (head (filter (\(x,y) -> x+y == 2020) (cartProd input input))))) -- first star
+    putStrLn (show (multTupleElements3 (head (filter (\(x,y,z) -> x+y+z == 2020) (cartProd3 input input input))))) -- second star
+    hClose handle
